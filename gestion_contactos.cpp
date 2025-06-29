4d43ed2217c0be4c229f821b5560dc95520e3dbb
ShirleyCauna <caunashirley11@gmail.com>
2025-06-28 21:33:04 -0500

    Versión final: 1. Gestión de Contactos - estructura contactoEmail con menú para agregar, eliminar, mostrar, ordenar por dominio y salir. Programa en ejecución continua hasta que el usuario indique salir.

--- gestion_contactos.cpp
+++ gestion_contactos.cpp
@@ -1,3 +1,16 @@
+/* 1. Gestión de Contactos
+Crear una estructura llamada contactoEmail que tenga las siguientes características: nombres completos,
+sexo, edad, teléfono, email, nacionalidad
+El programa debe constar de un menú que permita:
+a) Agregar un contacto
+b) Eliminar un contacto
+c) Mostrar listado general de contactos registrados hasta ese momento.
+d) Mostrar listado de contactos existentes, ordenado por servidor de correo de las cuentas (gmail.com,
+outlook.com, yahoo.com, etc.)
+e) Salir del programa
+El programa debe mantenerse en ejecución continuamente hasta que usuario indique que desea salir del
+mismo.*/
+
 #include <iostream>
 #include <cstring>
 
